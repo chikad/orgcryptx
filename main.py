8ecc1dedb24e30fd4d2d5f948c2e2c1fa9d58a2d
ckadi <31092158+chikad@users.noreply.github.com>
2022-04-16 18:13:33 -0700

    split out fetch contract into new object

--- main.py
+++ main.py
@@ -12,27 +12,23 @@ ABI_CACHE_PATH = "./abi_cache"
 AVAX_RPC = "https://api.avax.network/ext/bc/C/rpc"
 ETHE_RPC = f"https://eth-mainnet.alchemyapi.io/v2/{api_keys['ALCHEMY']}"
 
+AVAX_WEB3 = Web3(Web3.HTTPProvider(AVAX_RPC))
+ETHE_WEB3 = Web3(Web3.HTTPProvider(ETHE_RPC))
+
 class Transaction:
     def __init__(self):
         pass
 
-class TransactionOrganizer:
-    contract_cache = {}
-    avax_web3 = Web3(Web3.HTTPProvider(AVAX_RPC))
-    ethe_web3 = Web3(Web3.HTTPProvider(ETHE_RPC))
-
-    def __init__(self, address):
-        """
-        address (str) : get transactions for this address
-        """
-        if not TransactionOrganizer.contract_cache:
-            TransactionOrganizer.init_contract_cache()
-
-        self.address = address
-        self.network = None
+class ContractStorage:
+    initialized = False
 
     @classmethod
-    def init_contract_cache(cls):
+    def init_cache(cls):
+        if cls.initialized:
+            return
+
+        cls.cache = {}
+
         if not os.path.exists(ABI_CACHE_PATH):
             os.mkdir(ABI_CACHE_PATH)
 
@@ -41,30 +37,29 @@ class TransactionOrganizer:
                 abi = f.read()
 
             contract_address, network, _ = abi_file.split("_")
+            cls._switch_network(network)
+
             if abi:
-                if network == "AVAX":
-                    web3 = cls.avax_web3
-                elif network == "ETHE":
-                    web3 = cls.ethe_web3
-                else:
-                    assert False, f"unsupported network: {network}"
-
-                cls.contract_cache[contract_address] = web3.eth.contract(contract_address, abi=abi)
+                cls.cache[contract_address] = cls.web3.eth.contract(contract_address, abi=abi)
             else:
-                cls.contract_cache[contract_address] = web3.eth.contract(contract_address)
+                cls.cache[contract_address] = cls.web3.eth.contract(contract_address)
 
-    def set_network(self, network):
+        cls.initialized = True
+
+    @classmethod
+    def _switch_network(cls, network):
         if network == "AVAX":
-            self.web3 = TransactionOrganizer.avax_web3
+            cls.web3 = AVAX_WEB3
         elif network == "ETHE":
-            self.web3 = TransactionOrganizer.ethe_web3
+            cls.web3 = ETHE_WEB3
         else:
             assert False, f"unsupported network: {network}"
 
-        self.network = network
+        cls.network = network
 
-    def _get_contract(self, contract_address):
-        if self.network == "AVAX":
+    @classmethod
+    def _get_contract(cls, contract_address):
+        if cls.network == "AVAX":
             get_contract_abi_url = f"https://api.snowtrace.io/api?module=contract&action=getabi&address={contract_address}&apikey={api_keys['SNOWTRACE']}"
         else:
             get_contract_abi_url = f"https://api.etherscan.io/api?module=contract&action=getabi&address={contract_address}&apikey={api_keys['ETHERSCAN']}"
@@ -72,13 +67,14 @@ class TransactionOrganizer:
         abi = requests.get(get_contract_abi_url).json()["result"]
 
         if abi != "Contract source code not verified":
-            contract = self.web3.eth.contract(contract_address, abi=abi)
+            contract = cls.web3.eth.contract(contract_address, abi=abi)
         else:
-            contract = self.web3.eth.contract(contract_address)
+            contract = cls.web3.eth.contract(contract_address)
 
         return contract
 
-    def _get_impl_contract_for_proxy(self, contract):
+    @classmethod
+    def _get_impl_contract_for_proxy(cls, contract):
         """
         attempt to get the underlying implementation contract for a proxy contract
 
@@ -92,7 +88,7 @@ class TransactionOrganizer:
         # try reading the implementation variable (if it is public)
         try:
             impl_addr = contract.functions.implementation().call()
-            return self._get_contract(impl_addr)
+            return cls._get_contract(impl_addr)
         except ContractLogicError:
             # implementation function can only be called by admin,
             # so will try to get implementation from contract storage
@@ -109,7 +105,7 @@ class TransactionOrganizer:
         ]
 
         for impl_slot in potential_impl_slots:
-            impl_addr_bytes = self.web3.eth.get_storage_at(contract.address, impl_slot)
+            impl_addr_bytes = cls.web3.eth.get_storage_at(contract.address, impl_slot)
             if Web3.toInt(impl_addr_bytes) != 0:
                 break
         else:
@@ -118,28 +114,61 @@ class TransactionOrganizer:
 
         impl_addr = Web3.toChecksumAddress(Web3.toInt(impl_addr_bytes))
 
-        return self._get_contract(impl_addr)
+        return cls._get_contract(impl_addr)
 
+    @classmethod
+    def get_contract(cls, contract_address, network):
+        if not cls.initialized:
+            cls.init_cache()
 
-    def get_contract(self, contract_address):
+        cls._switch_network(network)
+
         # checks cache first, before making api call
-        contract = self.contract_cache.get(contract_address)
+        contract = cls.cache.get(contract_address)
         if contract:
             return contract
 
         # not in cache, make api call and load
-        contract = self._get_contract(contract_address)
+        contract = cls._get_contract(contract_address)
 
         if contract.abi and contract.find_functions_by_name("implementation"):
             # potential proxy contract, fetch the implementation contract
-            contract = self._get_impl_contract_for_proxy(contract)
+            contract = cls._get_impl_contract_for_proxy(contract)
 
-        with open(f"{ABI_CACHE_PATH}/{contract_address}_{self.network}_abi.json", "wt") as f:
+        with open(f"{ABI_CACHE_PATH}/{contract_address}_{cls.network}_abi.json", "wt") as f:
             if contract.abi:
                 json.dump(contract.abi, f, indent=4)
 
-        self.contract_cache[contract_address] = contract
+        cls.cache[contract_address] = contract
         return contract
+
+    @classmethod
+    def get_fn_name(cls, contract, fn_selector):
+        """
+        returns: the function name if verified abi exists,
+                 else, just returns the fn_selector
+        """
+        if contract.abi is not None:
+            return contract.get_function_by_selector(fn_selector).fn_name
+        else:
+            return fn_selector
+
+class TransactionOrganizer:
+    def __init__(self, address):
+        """
+        address (str) : get transactions for this address
+        """
+        self.address = address
+
+    def _switch_network(self, network):
+        if network == "AVAX":
+            self.web3 = AVAX_WEB3
+        elif network == "ETHE":
+            self.web3 = ETHE_WEB3
+        else:
+            assert False, f"unsupported network: {network}"
+
+        self.network = network
 
     def get_transactions_old(self):
         """
@@ -161,10 +190,7 @@ class TransactionOrganizer:
         """
         returns list of transaction hashes, pulling from covalent
         """
-        if self.network == "AVAX":
-            chain_id = 43114
-        else:
-            chain_id = 1
+        chain_id = 43114 if self.network == "AVAX" else 1
 
         get_txns_url = f"https://api.covalenthq.com/v1/{chain_id}/address/{self.address}/transactions_v2/?&key={api_keys['COVALENT']}"
         res = requests.get(get_txns_url).json()["data"]
@@ -176,10 +202,10 @@ class TransactionOrganizer:
         return txns
 
     def extract(self, network):
-        self.set_network(network)
+        self._switch_network(network)
         txns = self.get_transactions()
 
-        print(f"FOUND {len(txns)} TRANSACTIONS FOR ADDRESS {self.address}")
+        print(f"FOUND {len(txns)} {self.network} TRANSACTIONS FOR ADDRESS {self.address}")
         print(f"DISPLAYING FROM EARLIEST TO LATEST\n")
 
         for txn_hash in txns:
@@ -192,28 +218,40 @@ class TransactionOrganizer:
                 print("[SELF]")
 
             elif from_address == self.address:
-                contract = self.get_contract(to_address)
-                if contract.abi is not None:
-                    fn = contract.get_function_by_selector(fn_selector).fn_name
+                contract = ContractStorage.get_contract(to_address, self.network)
+                fn = ContractStorage.get_fn_name(contract, fn_selector)
+                if fn == "transfer":
+                    token_symbol = contract.functions.symbol().call()
+                    print("[ OUT]", to_address, fn, token_symbol)
                 else:
-                    fn = fn_selector
-                print("[ OUT]", to_address, fn)
+                    print("[ OUT]", to_address, fn)
 
             elif to_address == self.address:
-                # inflow
+                # transfer from a non-contract address
                 print(f"[  IN] {from_address} (transferred from)")
 
             else:
-                # someone else interacts with a contract
-                #   during contract execution, tokens are transferred to me
+                # contract address cannot be in [from] address
+                # contract cannot initiate a send, requires interaction from a user
+                #   a person -> interacts with -> a contract
+                #     -> during contract execution, tokens are transferred to me
+                #
                 # for example,
                 #   > Coinbase calls Transfer on an ERC20 token contract
                 #   > the ERC20 token(s) appear at my address
 
                 # etherscan / snowtrace doesn't show these interactions when listing transactions,
                 #   but they can be obtained by querying ERC20 token transfers
-                print(f"[  IN] {from_address}")
 
+                contract = ContractStorage.get_contract(to_address, self.network)
+                fn = ContractStorage.get_fn_name(contract, fn_selector)
+
+                if fn == "transfer":
+                    # likely an ERC20 token contract interaction
+                    token_symbol = contract.functions.symbol().call()
+                    print(f"[ *IN]", from_address, fn, token_symbol)
+                else:
+                    print(f"[ *IN]", from_address, fn)
 
         print()
 
